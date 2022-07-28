ee63ab889a188a0cf8024c386c04bd0a13c28e11
Jeff Kirner <jeff.kirner@gmail.com>
2022-07-28 11:45:20 -0700

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import BeersDirectoryPage from './pages/BeersDirectoryPage';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <BeersDirectoryPage />
+      <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='contact' element={<ContactPage />} />
+            <Route path='directory' element={<BeersDirectoryPage />} />
+        </Routes>
       <Footer />
     </div>
   );
