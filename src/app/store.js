c1d64278d66c416be6f59b58a6f2cb3bf3303729
Jeff Kirner <jeff.kirner@gmail.com>
2022-08-19 16:39:14 -0700

    connecting to Redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { beersReducer } from '../features/beers/beersSlice';
+import { promotionsReducer } from '../features/promotions/promotionsSlice';
 
 export const store = configureStore({
   reducer: {
     beers: beersReducer,
+    promotions: promotionsReducer
   },
 });
